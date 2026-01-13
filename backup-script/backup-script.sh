f8009e2d12bc49eee28d46172ba5873a5de6fc3c
NePalladin <iliya.velikiy@gmail.com>
2026-01-13 05:52:33 +0200

    add smart backup script with logging and env checks

--- backup-script/backup-script.sh
+++ backup-script/backup-script.sh
@@ -2,16 +2,16 @@
 
 echo "V0.2"
 
-: "${DEPLOY_MODE:?Ошибка: Переменная DEPLOY_MODE  должна быть установленна!}"
+: "${DEPLOY_MODE:?Ошибка: Переменная DEPLOY_MODE  должна быть установленна!}" #проверяем установленна ли переменная окружения DEPLOY_MODE 
 
-log_dir=$1
+log_dir=$1 #оглашаем переменную и присваем ей значение переданное при запуске скрипта
 
-function log_action()
+function log_action() #функция для логирования копирования файлов
 {
 echo "[$(date +%Y-%m-%d_%H:%M:%S)] $1"
 }
 
-function target_dir()
+function target_dir() #функция проверяющяя существует ли целевая дирректория для копирования, если нет, то создает ее
 {
 echo "checking if /tmp/backup/$1 is exist"
 if [ -d /tmp/backup/$1 ]
@@ -21,20 +21,33 @@ else
 	echo "creating directory /tmp/backup/$1 "
 	mkdir -p  /tmp/backup/$1
 fi
+}
+
+function copy_files()
+{
+for file in "$1"/*
+do
+	cp "$file" /tmp/backup/$2
+	log_action "Скопирован файл $file"
+done
 
 }
 
-if [ -d "$log_dir" ]
+if [ -d "$log_dir" ] #проверка существует ли дирректория из которой мы копируем и директория ли это вообще
 then 
 	if [ "$DEPLOY_MODE" = "PROD" ]
 	then
 		echo "deploy mode = prod"
 		target_dir "$DEPLOY_MODE"
-	elif [ "$DEPLOY_MODE" = "DEV" ]
-	then 
+		copy_files $log_dir $DEPLOY_MODE
+	else
 		echo " deploy mode = dev"
 		target_dir "$DEPLOY_MODE"
+		copy_files $log_dir $DEPLOY_MODE
+
 	fi
 else
 	echo "is not dir or it not exsist"
 fi
+
+echo "Скрипт завершил работу с кодом $?"
